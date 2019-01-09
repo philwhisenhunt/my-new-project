b71a17df38da06fcb59b5007d53809d56858911c
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-09 12:17:21 -0500

    Version seems to match, but isn't pulling correct url

--- main.js
+++ main.js
@@ -2,20 +2,25 @@
 const Apify = require('apify');
 
 //request the page that you want
-const Apify = require('apify');
-
 Apify.main(async () => {
     //create the request queue instance.
     const requestQueue = await Apify.openRequestQueue();
     // then, and ONLY then, can we add a request to it
     await requestQueue.addRequest({ url: 'https://www.apify.com'});
 
-});
 
-consta handlePageFunction = async ({ request, $ }) => {
+
+const handlePageFunction = async ({ request, $ }) => {
     const title = $('title').text();
     console.log(`The title of "${request.url}" is: ${title}.`);
 
 }
 
 //Set up the crawler, passing a single options object as an argument. 
+const crawler = new Apify.CheerioCrawler({
+    requestQueue,
+    handlePageFunction
+});
+
+await crawler.run();
+});
