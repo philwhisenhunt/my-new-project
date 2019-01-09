c2396c89408e82b2ef572a256a78f1aa54f1320a
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-09 13:57:35 -0500

    Version doesn't quite work, need to target things correctly

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ Apify.main(async () => {
     //create the request queue instance.
     const requestQueue = await Apify.openRequestQueue();
     // then, and ONLY then, can we add a request to it
-    await requestQueue.addRequest({ url: 'https://www.apify.com'});
+    await requestQueue.addRequest({ url: 'https://www.visitrichmondva.com/event/rva-on-ice/17800/'});
 
 
 
 const handlePageFunction = async ({ request, $ }) => {
     const title = $('title').text();
+    const location = $('.location').text();
     console.log(`The title of "${request.url}" is: ${title}.`);
+    console.log(`The location of "${request.url}" is: ${location}.`);
 
 }
 
